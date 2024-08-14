3aceb8287ac680d13f2867f36db6133687115160
Kristoffer Kuwaja <kristoffer.kuvaja@gmail.com>
2024-08-14 10:06:56 +0300

    updated id to use uuid

--- pkgs/eventtracker.py
+++ pkgs/eventtracker.py
@@ -1,6 +1,6 @@
 import csv
 import time
-from hashlib import sha256
+from uuid import uuid1
 
 class EventTracker():
     ''' Use to track events and to write them to a csv file
@@ -10,11 +10,10 @@ class EventTracker():
     def __init__(self, csvfile="eggs.csv") -> None:
         self.__events = []
         self.csvfile = csvfile
-        self.__runtime_start = time.time()
         self.__id = self.__set_id()
     
     def __set_id(self):
-        return sha256(str(self.__runtime_start).encode('utf-8')).hexdigest()
+        return uuid1()
 
     def addEvent(self, action, content):
         self.__events.append(self.__newEvent(action, content))
